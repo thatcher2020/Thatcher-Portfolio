ba8c306aa74bc704018d0fd08a4fad3e8dfa0bc1
Joseph Thatcher <thatcherjoe20@gmail.com>
2023-11-09 14:31:43 -0500

    pushing so laptop can update

--- project2/tech-blog/db/schema.sql
+++
@@ -1,3 +0,0 @@
-DROP DATABASE IF EXISTS tech_blog;
-
-CREATE DATABASE tech_blog;
--- project2/tech-blog/utils/helpers.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  format_date: (date) => {
-    const dateObj = new Date();
-    return `${dateObj.getUTCMonth() + 1}/${dateObj.getUTCDate() - 1}/${dateObj.getUTCFullYear()}`;
-  },
-};
